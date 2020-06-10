1892569e5a14efdcd4828d17c751caf7ebe41bdd
amayaleo <amayaleod@yahoo.com>
2020-06-10 12:02:32 -0300

    variables locales

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,15 @@
 extern "C" {
     #include <tinyPTC/src/tinyptc.h>
 }
-#include <cstdint> // uint32_t
-//#include <iostream>
+#include <cstdint>
+#include <iostream>
 
-//using namespace std;
 
-//crear un array
 
+int main(){
 int  width = 480;
 int height = 272;
 uint32_t screen[480 * 272];
-
-int main(){
-
 ptc_open("Juego",width,height);
 while(true){
 for(uint32_t i=0;i < (480*272);++i){
@@ -24,6 +20,6 @@ for(uint32_t i=0;i < (480*272);++i){
 }
 ptc_close();
 
-//cout<< "my first program gamer " <<endl;
+std::cout<< "my first program gamer " << std::endl;
 return 0;
 }
